777e2c4da4735107602621de2bbd24f78780df1a
Luciano Alves <lasbr_alves@outlook.com>
2023-05-11 19:25:59 -0300

    Added GameList

--- src/main/java/br/com/lasbr/gamelist/controllers/GameController.java
+++ src/main/java/br/com/lasbr/gamelist/controllers/GameController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
     @RestController
-    @RequestMapping(value = "/games")
+    @RequestMapping("/games")
     public class GameController {
 
         @Autowired
