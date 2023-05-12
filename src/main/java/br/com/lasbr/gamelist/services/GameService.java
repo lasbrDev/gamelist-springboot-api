2c601df4b5692b0e22dc8a6b86cbbbca082136ee
Luciano Alves <lasbr_alves@outlook.com>
2023-05-12 09:39:00 -0300

    Added instance of GameDto in GameService

--- src/main/java/br/com/lasbr/gamelist/services/GameService.java
+++ src/main/java/br/com/lasbr/gamelist/services/GameService.java
@@ -1,10 +1,12 @@
 package br.com.lasbr.gamelist.services;
 
+import br.com.lasbr.gamelist.dto.GameDTO;
 import br.com.lasbr.gamelist.dto.GameMinDTO;
 import br.com.lasbr.gamelist.entities.Game;
 import br.com.lasbr.gamelist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,6 +15,12 @@ import java.util.List;
 
         @Autowired
         private GameRepository gameRepository;
+        @Transactional(readOnly = true)
+        public GameDTO findById(Long id) {
+            Game result = gameRepository.findById(null).get();
+            return new GameDTO(result);
+        }
+        @Transactional(readOnly = true)
         public List<GameMinDTO> findAll() {
             List<Game> result = gameRepository.findAll();
             return result.stream().map(x -> new GameMinDTO(x)).toList();
