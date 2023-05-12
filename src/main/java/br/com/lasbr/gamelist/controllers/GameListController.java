d2254b837d08e8a2dd0d3b1ab7f44ab166ed3a85
Luciano Alves <lasbr_alves@outlook.com>
2023-05-12 11:44:42 -0300

    Added SQL queries, added projections interface

--- src/main/java/br/com/lasbr/gamelist/controllers/GameListController.java
+++ src/main/java/br/com/lasbr/gamelist/controllers/GameListController.java
@@ -2,9 +2,12 @@ package br.com.lasbr.gamelist.controllers;
 
 
 import br.com.lasbr.gamelist.dto.GameListDTO;
+import br.com.lasbr.gamelist.dto.GameMinDTO;
 import br.com.lasbr.gamelist.services.GameListService;
+import br.com.lasbr.gamelist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,9 +20,18 @@ import java.util.List;
         @Autowired
         private GameListService gameListService;
 
+        @Autowired
+        private GameService gameService;
+
         @GetMapping
         public List<GameListDTO> findAll() {
             List<GameListDTO> result = gameListService.findAll();
+            return result;
+        }
+
+        @GetMapping("/{listId}/games")
+        public List<GameMinDTO> findByList(@PathVariable Long listId) {
+            List<GameMinDTO> result = gameService.findByList(listId);
             return result;
         }
     }
--- src/main/java/br/com/lasbr/gamelist/services/GameService.java
+++ src/main/java/br/com/lasbr/gamelist/services/GameService.java
@@ -3,6 +3,7 @@ package br.com.lasbr.gamelist.services;
 import br.com.lasbr.gamelist.dto.GameDTO;
 import br.com.lasbr.gamelist.dto.GameMinDTO;
 import br.com.lasbr.gamelist.entities.Game;
+import br.com.lasbr.gamelist.projections.GameMinProjection;
 import br.com.lasbr.gamelist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -24,5 +25,10 @@ import java.util.List;
         public List<GameMinDTO> findAll() {
             List<Game> result = gameRepository.findAll();
             return result.stream().map(x -> new GameMinDTO(x)).toList();
+        }
+
+        public List<GameMinDTO> findByList(Long listId) {
+            List<GameMinProjection> result = gameRepository.searchByList(listId);
+            return  result.stream().map(x -> new GameMinDTO(x)).toList();
         }
     }
