768246438f43ae212e832ff8d5b4ff3b0e5eda03
Luciano Alves <lasbr_alves@outlook.com>
2023-05-12 09:46:07 -0300

    Added endpoint to GameController

--- src/main/java/br/com/lasbr/gamelist/controllers/GameController.java
+++ src/main/java/br/com/lasbr/gamelist/controllers/GameController.java
@@ -1,11 +1,13 @@
 package br.com.lasbr.gamelist.controllers;
 
+import br.com.lasbr.gamelist.dto.GameDTO;
 import br.com.lasbr.gamelist.dto.GameMinDTO;
 import br.com.lasbr.gamelist.entities.Game;
 import br.com.lasbr.gamelist.repositories.GameRepository;
 import br.com.lasbr.gamelist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +21,12 @@ import java.util.List;
         private GameRepository gameRepository;
         @Autowired
         private GameService gameService;
+
+        @GetMapping("/{id}")
+        public GameDTO findById(@PathVariable Long id) {
+            GameDTO result = gameService.findById(id);
+            return result;
+        }
 
         @GetMapping
         public List<GameMinDTO> findAll() {
